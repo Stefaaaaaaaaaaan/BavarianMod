62604b237ef386ddf19b0521b7d0646df5dec393
Juliletspluy <Juliletspluy@github.com>
2020-04-26 18:15:48 +0200

    Adding general data

---
+++ src/main/java/com/Juliletspluy/bavarianMod/Main.java
@@ -0,0 +1,32 @@
+package com.Juliletspluy.bavarianMod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(Main.MOD_ID)
+public class Main
+{
+    public static Main instance;
+    public static final String MOD_ID = "bavarianMod";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Main() {
+        instance = this;
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+    private void clientSetup(final FMLClientSetupEvent event){ }
+    public void onServerStarting(FMLServerStartingEvent event){ }
+
+}
